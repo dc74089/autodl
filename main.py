b68d4e83c468f92e04b3b10c67638fccbed34857
Dominic Canora <dominiccanora@gmail.com>
2024-08-25 14:48:09 -0400

    Take in directory on command line

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+import argparse
 import os
 
 from yt_dlp import YoutubeDL
 
-
 tempdir = os.path.expanduser("~/.ydl-tmp/")
 os.makedirs(tempdir, exist_ok=True)
-
 
 
 def do_download(directory, url):
@@ -24,24 +23,33 @@ def do_download(directory, url):
                             'only_multi_video': True,
                             'when': 'playlist'}],
         'retries': 10,
-        'simulate': False
+        'simulate': True
     }
 
     with YoutubeDL(opts) as ydl:
         ydl.download([url])
 
 
-# Get the current working directory
-current_dir = os.getcwd()
+def main():
+    # Set up command-line argument parsing
+    parser = argparse.ArgumentParser(description="Process 'urls.txt' files in subdirectories.")
+    parser.add_argument('root_dir', help="The root directory to start searching from.")
+    args = parser.parse_args()
 
-# Iterate over all subdirectories in the current directory
-for root, dirs, files in os.walk(current_dir):
-    for dir_name in dirs:
-        sub_dir_path = os.path.join(root, dir_name)
-        urls_file_path = os.path.join(sub_dir_path, 'urls.txt')
+    root_dir = args.root_dir
 
-        # Check if 'urls.txt' exists in this subdirectory
-        if os.path.isfile(urls_file_path):
-            with open(urls_file_path, 'r') as file:
-                for line in file:
-                    do_download(sub_dir_path, line)
+    # Iterate over all subdirectories in the current directory
+    for root, dirs, files in os.walk(root_dir):
+        for dir_name in dirs:
+            sub_dir_path = os.path.join(root, dir_name)
+            urls_file_path = os.path.join(sub_dir_path, 'urls.txt')
+
+            # Check if 'urls.txt' exists in this subdirectory
+            if os.path.isfile(urls_file_path):
+                with open(urls_file_path, 'r') as file:
+                    for line in file:
+                        do_download(sub_dir_path, line)
+
+
+if __name__ == '__main__':
+    main()
